fdff75e91b593a64c75aa21a9514436b0717db03
Ian Whitehouse <iw9892a@american.edu>
2024-03-26 18:23:12 -0400

    Completed project to generate images of fires in trees

---
+++ generate_image_data.py
@@ -0,0 +1,56 @@
+# Imports
+from diffusers import StableDiffusionPipeline, StableDiffusionInpaintPipeline
+import numpy as np
+from PIL import Image
+
+
+# Load diffusion models
+background_model = StableDiffusionPipeline.from_pretrained(
+	"stabilityai/stable-diffusion-2",
+).to("cuda")
+inpainting_model = StableDiffusionInpaintPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-2-inpainting"
+).to("cuda")
+
+default_negative_prompt = None # "low quality, poor quality, anime, cartoon, ugly, deformed, disfigured, poor details, blurry"
+
+# Generate background img or img(s)
+def gen_background_img(prompt, negative_prompt=default_negative_prompt, num_imgs_per_prompt=8, width=640, height=480):
+    return background_model(prompt, negative_prompt=negative_prompt, num_images_per_prompt=num_imgs_per_prompt, width=width, height=height, guidance_scale=10).images
+
+# Generate bounding boxes (aspect_ratio is in width/height)
+def gen_bounding_box_img(
+    aspect_ratio_min=None, aspect_ratio_max=None,
+    width_min=None, height_min=None, width_max=None, height_max=None,
+    img_width=640, img_height=480
+):
+    # Set the width and height if it the aspect ratio is defined
+    if aspect_ratio_min and aspect_ratio_max:
+        assert not (width_min or height_min or width_max or height_max)
+        aspect_ratio = np.random.uniform(low=aspect_ratio_min, high=aspect_ratio_max)
+
+    if width_min and width_max and height_min and height_max:
+        assert not (aspect_ratio_min or aspect_ratio_max)
+        width = np.random.randint(width_min, width_max)
+        height = np.random.randint(height_min, height_max)
+    else:
+        if aspect_ratio < 1:
+            height = np.random.randint(0, img_height)
+            width = height * aspect_ratio
+        else:
+            width = np.random.randint(0, img_width)
+            height = width/aspect_ratio
+
+    position_x = np.random.randint(0, img_width-width)
+    position_y = np.random.randint(0, img_height-height)
+
+    img = np.zeros((img_height, img_width, 1))
+    print(position_y, int(position_y + height), position_x, position_x + width)
+    img[position_y:int(position_y + height), position_x:int(position_x + width), :] = 1
+    return img
+
+# Generate inpainted images (all of these should be single values)
+def inpaint_class_into_images(image, bounding_box_information, class_prompt, negative_prompt=default_negative_prompt, width=640, height=480):
+    bounding_box = gen_bounding_box_img(**bounding_box_information)
+    print(class_prompt)
+    return inpainting_model(prompt=class_prompt, negative_prompt=negative_prompt, image=image, mask_image=bounding_box, width=width, height=height, guidance_scale=20, strength=0.75).images, bounding_box
---
+++ generate_multiclass_dataset.py
@@ -0,0 +1,44 @@
+# Imports
+import pandas as pd
+from generate_image_data import gen_background_img, inpaint_class_into_images
+from random import randint
+import numpy as np
+from PIL import Image
+
+
+# Load list of backgrounds
+backgrounds = pd.read_csv("backgrounds.csv").to_dict("records")
+
+# Generate backgrounds
+background_imgs = []
+for bg in backgrounds:
+    background_imgs += gen_background_img(bg["prompt"], num_imgs_per_prompt=bg["num_imgs"])
+
+# Load list of classes
+classes = pd.read_csv("classes.csv")
+classes = classes.replace(np.nan, None)
+classes = classes.to_dict("records")
+
+for i, img in enumerate(background_imgs):
+    bbs = []
+    img.save("output/bg_%d.png" % i)
+    for cls in classes:
+        cls_bbs = []
+        cls_instances = randint(cls["min_appear"], cls["max_appear"])
+        for num_cls in range(cls_instances):
+            print({k: cls[k] for k in ["aspect_ratio_min", "aspect_ratio_max", "width_min", "height_min", "width_max", "height_max"]})
+            new_img, bb = inpaint_class_into_images(
+                img,
+                {k: cls[k] for k in ["aspect_ratio_min", "aspect_ratio_max", "width_min", "height_min", "width_max", "height_max"]},
+                cls["prompt"]
+            )                
+            img = new_img[0]
+            cls_bbs.append(bb.tolist())
+            
+        if cls_instances < 1:
+            bbs.append(np.zeros((480, 640)))
+        else:
+            bbs.append(np.any(np.array(cls_bbs), axis=0))
+        
+    img.save("output/%d_x.png" % i)
+    np.save("output/%d_y.npy" % i, np.stack(bbs, axis=-1))
